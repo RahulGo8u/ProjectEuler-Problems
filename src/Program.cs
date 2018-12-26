8fc08893ea22224e4d43c3a3a42182b273386409
Anand Rahul <rahul.anand@gemalto.com>
2018-12-26 16:27:47 +0530

    Problem1, Multiples of 3 and 5

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectEuler.Problems;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,12 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
+        public static void Main(string[] args)
+        {            
+            Console.WriteLine(Problem1.GetMultiplesOf3or5(1000));
+            Console.ReadLine();
         }
     }
 }
