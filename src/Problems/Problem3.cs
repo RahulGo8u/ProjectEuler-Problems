a02f2c887307ffda665a88d644abeb5b24d2eeeb
Anand Rahul <rahul.anand@gemalto.com>
2018-12-27 23:13:26 +0530

    Problem 3, Largest prime factor

---
+++ src/Problems/Problem3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems
+{
+    /*
+    Largest prime factor
+    Problem 3 
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+     */
+    class Problem3
+    {                
+        /*48
+        48%2==0(24)
+        48%3==0(16)
+        48%4==0(12)
+        48%6==0(8)
+        48%8==0(6)
+        48%12==0(4)
+        48%16==0(3)*/
+        //Check only that if factor is primenumber or not. This is very fast to test.
+        internal static long LargestPrimeFactor(long number)
+        {
+            long i = 2, factor=1;
+            while(true)
+            {
+                if (number % i == 0)
+                {
+                    factor = number / i;
+                    if (IsPrimeNumber(factor))
+                    {
+                        return factor;
+                    }
+                }                
+                i++;
+            }            
+        }
+        private static bool IsPrimeNumber(long number)
+        {
+            long divisor = 2;
+            for (long i = number / 2; i > 1; i+=0)
+            {
+                if (number % i == 0) return false;
+                i = number / (++divisor);
+            }
+            return true;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -10,9 +10,10 @@ namespace ProjectEuler
     public class Program
     {
         public static void Main(string[] args)
-        {            
+        {
             Console.WriteLine(Problem1.GetMultiplesOf3or5(1000));
             Console.WriteLine(Problem2.EvenFibonacciNumbers(4000000));
+            Console.WriteLine(Problem3.LargestPrimeFactor(600851475143));            
             Console.ReadLine();
         }
     }
